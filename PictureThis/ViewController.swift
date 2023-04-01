e14c4172af8db76cf8c9b0e6716569fec1b64f04
Jean-Pierre Ciotta <jpciotta@gmail.com>
2023-04-01 12:22:07 -0400

    Sign up works but login is bugging

--- PictureThis/ViewController.swift
+++ PictureThis/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
     
@@ -13,6 +15,68 @@ class ViewController: UIViewController {
         
     }
 
+    @IBOutlet weak var usernameLabel: UITextField!
+        
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        guard let username = usernameLabel.text,
+              let password = passwordLabel.text,
+              !username.isEmpty,
+              !password.isEmpty
+                
+        else {
+            print("username or pasword is nil!")
+            return
+        }
+        
+        Firebase.Auth.auth().signIn(withEmail: username, password: password) {
+            result, error in if let e = error{
+                print(e.localizedDescription)
+                return
+                
+            }
+            guard let res = result else {
+                print("Error occured with ligging in ")
+                return
+            }
+            print("Signed in as \(res.user.email)")
+            self.performSegue(withIdentifier: "loginSeque", sender: nil)
+        }
+    }
+    // sign up
+    
+    
+    @IBOutlet weak var signUpUsernameLabel: UITextField!
+    
+    @IBOutlet weak var signUpPasswordLabel: UITextField!
+    
+    @IBOutlet weak var signUpSecondPasswordLabel: UITextField!
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        guard let username = signUpUsernameLabel.text,
+              let password = signUpPasswordLabel.text else {
+            print("username or pasword is nil!")
+            return
+        }
+        
+        Firebase.Auth.auth().createUser(withEmail: username, password: password) {
+            result, error in if let e = error{
+                print(e.localizedDescription)
+                return
+                
+            }
+            guard let res = result else {
+                print("Error occured with ligging in ")
+                return
+            }
+            print("Signed in as \(res.user.email)")
+            self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
