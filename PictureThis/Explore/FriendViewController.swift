bf26ef3493c1aef3b08aef609bc81f7431df8156
Harjyot Badh <badhHarjyot99@live.com>
2023-04-06 23:09:47 -0400

    Friend Stuff

--- PictureThis/Explore/FriendViewController.swift
+++ PictureThis/Explore/FriendViewController.swift
@@ -18,7 +18,7 @@ class FriendViewController: UIViewController {
         super.viewDidLoad()
         
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.//
     }
     
     
